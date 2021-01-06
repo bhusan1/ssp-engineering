b6d8ebd53c1cc6bb6e1982ccb99280a1e6e65b6e
Bhusan <43221027+bhusan1@users.noreply.github.com>
2021-01-06 08:46:43 -0600

    Add files via upload

---
+++ src/app/components/all-projects/all-projects.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-all-projects',
+  templateUrl: './all-projects.component.html',
+  styleUrls: ['./all-projects.component.css']
+})
+export class AllProjectsComponent implements OnInit {
+  title = 'All Projects';
+  noOfItems = 'all';
+
+  constructor() {}
+
+  ngOnInit() {}
+}
