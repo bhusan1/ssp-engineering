0e856a1bf276fb4017ce07bf2642d927bc3687e0
Bhusan <bhusan1@github.com>
2021-01-06 10:56:39 -0600

    running changes

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyA9pg_jJZCprGjFffzotIElI4bVdiLkzXc",
+  authDomain: "ssp-engineering.firebaseapp.com",
+  databaseURL: "https://ssp-engineering.firebaseio.com",
+  projectId: "ssp-engineering",
+  storageBucket: "ssp-engineering.appspot.com",
+  messagingSenderId: "1053071987362",
+  appId: "1:1053071987362:web:ee087036d8942131"
+};
