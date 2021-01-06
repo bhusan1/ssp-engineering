c6a6ce493d3dcb1b3e127f56b274ae48e3ab22c0
Bhusan <43221027+bhusan1@users.noreply.github.com>
2021-01-06 08:39:11 -0600

    Add files via upload

---
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Output, EventEmitter, ChangeDetectorRef, OnDestroy } from '@angular/core';
+import { AuthService } from 'app/services/auth.service';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css'],
+  providers: [AuthService]
+})
+export class DashboardComponent implements OnInit, OnDestroy {
+  @Output() userAuthStatus = new EventEmitter<any>();
+
+  constructor(public authService: AuthService, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  userStatus = this.authService.userStatus;
+  mobileQuery: MediaQueryList;
+
+  private _mobileQueryListener: () => void;
+
+  ngOnInit() {
+    this.authService.userChanges();
+    this.authService.userStatusChanges.subscribe((x) => {
+      this.userStatus = x;
+      this.userAuthStatus.emit(this.userStatus);
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+}
