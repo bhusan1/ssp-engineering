98d3c4840a80bec3c1121b4d47bc27d054209c5e
Bhusan <43221027+bhusan1@users.noreply.github.com>
2021-01-06 08:45:23 -0600

    Add files via upload

---
+++ src/app/components/admin/view-services/view-services.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { FirebaseService } from 'app/services/firebase.service';
+import { MatTableDataSource } from '@angular/material/table';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+
+@Component({
+  selector: 'app-view-services',
+  templateUrl: './view-services.component.html',
+  styleUrls: ['./view-services.component.css'],
+  providers: [FirebaseService],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition(
+        'expanded <=> collapsed',
+        animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')
+      )
+    ])
+  ]
+})
+export class ViewServicesComponent implements OnInit {
+
+  displayedColumns = [
+    'title',
+    'serviceCategory',
+    'imageSrc',
+    'description',
+    'updatedAt',
+    'actions'
+  ];
+  dataSource: MatTableDataSource<any>;
+  noServicesData = true;
+
+  constructor(private firebaseService: FirebaseService) { }
+
+  ngOnInit() {
+    this.getAllProjects();
+  }
+
+  getAllProjects() {
+    this.firebaseService
+      .getFirestoreCollection('/services-test', ref => ref.where('serviceCategory', '==', 'industrial'))
+      .valueChanges()
+      .subscribe(ServicesData => {
+        if (ServicesData) {
+          console.log(ServicesData);
+          this.noServicesData = false;
+          this.dataSource = new MatTableDataSource(ServicesData);
+        }
+        else {
+          this.noServicesData = true;
+        }
+      });
+  }
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
+
+
+
